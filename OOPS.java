ed6d1c9a30e68ff3c6e98a62edbc330a087399ba
nnaveen20242-cpu <nnaveen20242@gmail.com>
2026-03-02 14:11:19 +0530

    UC 1 added

--- OOPS.java
+++ OOPS.java
@@ -1,7 +1,18 @@
+//This is UC 1
+
+/*
+
+@author Naveen
+@version 1.1
+*/
+
+
 public class OOPS {
     public static void main(String[] args) {
         // Your Code goes here!
         System.out.println("Hello");
+
+        System.out.println("TOOPS Print");
 
     }
 }
